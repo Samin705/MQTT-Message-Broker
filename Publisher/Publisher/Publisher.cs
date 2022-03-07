692efe418296224b33606654b4a9cf69a3588b48
SaminCodeMen <codemen.samin@gmail.com>
2022-03-07 11:43:24 +0600

    first commit

---
+++ Publisher/Publisher/Publisher.cs
@@ -0,0 +1,51 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using System;
+
+public class Publisher
+{
+    static async Task Main(string[] args)
+    {
+        var mqttfactory = new MqttFactory();
+        IMqttClient client = mqttfactory.CreateMqttClient();
+        var options = new MqttClientOptionsBuilder()
+                          .WithClientId(Guid.NewGuid().ToString())
+                          .WithTcpServer("test.mosquitto.org", 1883)
+                          .WithCleanSession()
+                          .Build();
+        client.UseConnectedHandler(e =>
+        {
+            Console.WriteLine("Connected to the broker successfully");
+        });
+
+        client.UseDisconnectedHandler(e =>
+        {
+            Console.WriteLine("Disconnected to the broker successfully");
+        });
+
+        await client.ConnectAsync(options);
+
+        Console.WriteLine("Pres a key to publish the message");
+        Console.ReadLine();
+
+        await PublishMessageAsync(client);
+
+      await client.DisconnectAsync();
+    }
+
+    private static async Task PublishMessageAsync(IMqttClient client)
+    {
+        string messagePayLoad = "Hello";
+        var message = new MqttApplicationMessageBuilder()
+                          .WithTopic("Samin")
+                          .WithPayload(messagePayLoad)
+                          .WithAtLeastOnceQoS()
+                          .Build();
+
+        if(client.IsConnected)
+        {
+           await client.PublishAsync(message);
+        }
+    }
+}
---
+++ Publisher/Subscriber/Subscriber.cs
@@ -0,0 +1,48 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using System.Text;
+
+public class Subscriber
+{
+    static async Task Main(string[] args)
+    {
+        var mqttfactory = new MqttFactory();
+        IMqttClient client = mqttfactory.CreateMqttClient();
+        var options = new MqttClientOptionsBuilder()
+                          .WithClientId(Guid.NewGuid().ToString())
+                          .WithTcpServer("test.mosquitto.org", 1883)
+                          .WithCleanSession()
+                          .Build();
+
+        client.UseConnectedHandler(async e =>
+        {
+            Console.WriteLine("Connected to the broker successfully");
+            var topicBuilder = new TopicFilterBuilder()
+                                   .WithTopic("Samin")
+                                   .Build();
+
+            await client.SubscribeAsync(topicBuilder);
+        });
+
+        client.UseDisconnectedHandler(e =>
+        {
+            Console.WriteLine("Disconnected to the broker successfully");
+        });
+
+        client.UseApplicationMessageReceivedHandler(e =>
+        {
+            Console.WriteLine($"Received Message - {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+        });
+
+        await client.ConnectAsync(options);
+
+       
+        Console.ReadLine();
+
+        await client.DisconnectAsync();
+
+       
+    }
+
+}
